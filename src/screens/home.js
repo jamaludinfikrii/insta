799ea9e9d1e9d0c93d6fddeeba8376cd676bb28c
james <jamesfikrii@gmail.com>
2019-11-20 09:21:01 +0700

    post

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View,ScrollView } from 'react-native';
 import {Image,Avatar, Text} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 const data =[
     {username : 'fikri' , url_foto : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKrpS7K4U5ju_Jqtj69t2SW90P8G0yInzjmySwy-McoemFPXj0', caption : 'caption',likes : 10},
     {username : 'andi' , url_foto : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKrpS7K4U5ju_Jqtj69t2SW90P8G0yInzjmySwy-McoemFPXj0', caption : 'caption',likes : 10},
@@ -15,7 +17,8 @@ export default class home extends Component {
     return (
       <ScrollView style={{paddingTop:30}}>
         <View>
-            <View style={{flexDirection:'row',paddingHorizontal:15}}>
+            <View style={{flexDirection:'row',paddingHorizontal:15, justifyContent:'space-between' }}>
+              <View style={{flexDirection:'row'}}>
                 <Avatar
                     rounded
                     source={{
@@ -24,6 +27,14 @@ export default class home extends Component {
                     }}
                     />
                 <Text style={{marginLeft:15,fontWeight:'bold',fontSize:18}}>Username</Text>
+
+              </View>
+              <View style={{alignSelf:'center'}}>
+                <Icon 
+                  name='ellipsis-v'
+                  size ={20}
+                />
+              </View>
             </View>
 
 
@@ -33,9 +44,34 @@ export default class home extends Component {
                     style={{ width: 400,height:400}}
                 />
             </View>
+            <View style={{flexDirection:'row',paddingLeft:15,marginTop:15,justifyContent:'space-between'}}>
+              <View style={{flexDirection:'row'}}>
+                <Icon 
+                  style={{marginRight:20}}
+                  // style={{borderWidth:0.5,borderColor:'black'}}
+                  name='heart-o'
+                  size={30}
+                />
+                <Icon 
+                  // style={{borderWidth:0.5,borderColor:'black'}}
+                  name='comment-o'
+                  size={30}
+                />
+              </View>
+              <View>
+              <Icon 
+                  style={{marginRight:20}}
+                  // style={{borderWidth:0.5,borderColor:'black'}}
+                  name='tag'
+                  size={30}
+                />
+              </View>
+            </View>
 
             
-            <View></View>
+            <View style={{flexDirection:'row',marginTop:15,paddingHorizontal:15}}>
+              <Text style={{fontWeight:'bold'}}>Username <Text style={{fontWeight:"normal"}}>Ini Adalah Caption</Text></Text>
+            </View>
             <View></View>
         </View>
       </ScrollView>
