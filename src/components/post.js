41aa6bdbdc6612d21bc34ad3fea57ecb7efcfe7d
james <jamesfikrii@gmail.com>
2019-11-26 10:32:07 +0700

    home

---
+++ src/components/post.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import {Image,Avatar, Text} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+export default class post extends Component {
+  render() {
+    return (
+        <View style={{marginVertical:20}}>
+        {/* Profile Pict And Username */}
+        <View style={{flexDirection:'row',paddingHorizontal:15, justifyContent:'space-between' }}>
+          <View style={{flexDirection:'row'}}>
+            <Avatar
+                rounded
+                source={{
+                    uri:
+                    this.props.avatarUrl,
+                }}
+                />
+            <Text style={{marginLeft:15,fontWeight:'bold',fontSize:18}}>{this.props.username}</Text>
+
+          </View>
+          <View style={{alignSelf:'center'}}>
+            <Icon 
+              name='ellipsis-v'
+              size ={20}
+            />
+          </View>
+        </View>
+
+        {/* Image */}
+        <View style={{borderTopWidth:0.3, borderTopColor:'grey',marginTop:10}}>
+            <Image 
+                source={{uri : this.props.postUrl}}
+                style={{ width: '100%',height:400}}
+            />
+        </View>
+
+        {/* Icon Like Comments And Bookmark */}
+        <View style={{flexDirection:'row',paddingLeft:15,marginTop:15,justifyContent:'space-between'}}>
+          <View style={{flexDirection:'row'}}>
+            <Icon 
+              style={{marginRight:20}}
+              // style={{borderWidth:0.5,borderColor:'black'}}
+              name='heart-o'
+              size={30}
+            />
+            <Icon 
+              // style={{borderWidth:0.5,borderColor:'black'}}
+              name='comment-o'
+              size={30}
+            />
+          </View>
+          <View>
+          <Icon 
+              style={{marginRight:20}}
+              // style={{borderWidth:0.5,borderColor:'black'}}
+              name='tag'
+              size={30}
+            />
+          </View>
+        </View>
+
+        {/* Captions */}
+        <View style={{flexDirection:'row',marginTop:15,paddingHorizontal:15}}>
+          <Text style={{fontWeight:'bold'}}>{this.props.username} <Text style={{fontWeight:"normal"}}>{this.props.caption}</Text></Text>
+        </View>
+    </View>
+    );
+  }
+}
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View,ScrollView } from 'react-native';
+import { View,ScrollView, ActivityIndicator } from 'react-native';
 import {Image,Avatar, Text} from 'react-native-elements'
 import Icon from 'react-native-vector-icons/FontAwesome'
-
+import Post from './../components/post'
+import { urlApi } from '../supports/url';
+import Axios from 'axios';
 const data =[
     {username : 'fikri' , url_foto : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKrpS7K4U5ju_Jqtj69t2SW90P8G0yInzjmySwy-McoemFPXj0', caption : 'caption',likes : 10},
     {username : 'andi' , url_foto : 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQKrpS7K4U5ju_Jqtj69t2SW90P8G0yInzjmySwy-McoemFPXj0', caption : 'caption',likes : 10},
@@ -13,69 +15,36 @@ const data =[
 
 
 export default class home extends Component {
+  state = {data : null}
+  
+  componentDidMount(){
+    Axios.get(urlApi + 'post/getallpost')
+    .then((data) => {
+      this.setState({data:data.data.data})
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   render() {
+    if(this.state.data === null){
+      return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size='large' />
+      </View>
+      )
+    }
     return (
       <ScrollView style={{paddingTop:30}}>
-        <View>
-            {/* Profile Pict And Username */}
-            <View style={{flexDirection:'row',paddingHorizontal:15, justifyContent:'space-between' }}>
-              <View style={{flexDirection:'row'}}>
-                <Avatar
-                    rounded
-                    source={{
-                        uri:
-                        'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-                    }}
-                    />
-                <Text style={{marginLeft:15,fontWeight:'bold',fontSize:18}}>Username</Text>
-
-              </View>
-              <View style={{alignSelf:'center'}}>
-                <Icon 
-                  name='ellipsis-v'
-                  size ={20}
-                />
-              </View>
-            </View>
-
-            {/* Image */}
-            <View style={{borderTopWidth:0.3, borderTopColor:'grey',marginTop:10}}>
-                <Image 
-                    source={{uri : 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg'}}
-                    style={{ width: 400,height:400}}
-                />
-            </View>
-
-            {/* Icon Like Comments And Bookmark */}
-            <View style={{flexDirection:'row',paddingLeft:15,marginTop:15,justifyContent:'space-between'}}>
-              <View style={{flexDirection:'row'}}>
-                <Icon 
-                  style={{marginRight:20}}
-                  // style={{borderWidth:0.5,borderColor:'black'}}
-                  name='heart-o'
-                  size={30}
-                />
-                <Icon 
-                  // style={{borderWidth:0.5,borderColor:'black'}}
-                  name='comment-o'
-                  size={30}
-                />
-              </View>
-              <View>
-              <Icon 
-                  style={{marginRight:20}}
-                  // style={{borderWidth:0.5,borderColor:'black'}}
-                  name='tag'
-                  size={30}
-                />
-              </View>
-            </View>
-
-            {/* Captions */}
-            <View style={{flexDirection:'row',marginTop:15,paddingHorizontal:15}}>
-              <Text style={{fontWeight:'bold'}}>Username <Text style={{fontWeight:"normal"}}>Ini Adalah Caption</Text></Text>
-            </View>
-        </View>
+        {
+          this.state.data.map((val) => {
+            return(
+              <Post avatarUrl={urlApi + "public/profile/default.png"} postUrl = {urlApi + val.foto_url} username={val.username} caption={val.caption}/>
+            )
+          })
+        }
+        
       </ScrollView>
     );
   }
