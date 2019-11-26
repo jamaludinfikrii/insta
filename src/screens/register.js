5f07b8418b6ec5b195fa447205d5f3ecdc8e9654
james <jamesfikrii@gmail.com>
2019-11-26 08:39:27 +0700

    fb and google sign in

--- src/screens/register.js
+++ src/screens/register.js
@@ -7,6 +7,11 @@ import Axios from 'axios'
 import {onRegisterSuccess} from './../redux/actions/users'
 import { urlApi } from '../supports/url';
 import { StackActions,NavigationActions } from 'react-navigation'
+import {
+    GoogleSignin
+  } from '@react-native-community/google-signin';
+
+  import { LoginButton, AccessToken ,LoginManager} from 'react-native-fbsdk';
 
 class Register extends Component {
     state={
@@ -94,6 +99,16 @@ class Register extends Component {
         .catch((err) => {
 
         })
+
+        GoogleSignin.configure({
+            scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+            webClientId: '426889256865-ss0qtg72mhtm43gbjnoj79jijqhh3ngk.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+            offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+            hostedDomain: '', // specifies a hosted domain restriction
+            loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+            forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+            accountName: '', // [Android] specifies an account name on the device that should be used
+          });
     }
 
     componentDidUpdate(){
@@ -105,7 +120,50 @@ class Register extends Component {
             this.props.navigation.dispatch(reset_stack)
         }
     }
+    initUser = (token) => {
+        fetch('https://graph.facebook.com/v2.5/me?fields=email,name,friends&access_token=' + token)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json)
+          
+          // Some user object has been set up somewhere, build that user here
+        //   const user = {}
+        //   user.name = json.name
+        //   user.id = json.id
+        //   user.user_friends = json.friends
+        //   user.email = json.email
+        //   user.username = json.name
+        //   user.loading = false
+        //   user.loggedIn = true
+        //   user.avatar = setAvatar(json.id)      
+        //   console.log(user)  
+        })
+        .catch((err) => {
+            console.log('ERROR GETTING DATA FROM FACEBOOK')
+            console.log(err)
+        })
+      }
 
+    _signIn = async () => {
+        console.log('trigerred')
+        try {
+          await GoogleSignin.hasPlayServices();
+          const userInfo = await GoogleSignin.signIn();
+        //   this.setState({ userInfo });
+        console.log(userInfo)
+        } catch (error) {
+            console.log(error)
+        //   if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        //     // user cancelled the login flow
+        //   } else if (error.code === statusCodes.IN_PROGRESS) {
+        //     // operation (e.g. sign in) is in progress already
+        //   } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        //     // play services not available or outdated
+        //   } else {
+        //     // some other error happened
+        //   }
+        }
+      };
     render() {
     if(this.state.check_storage === false){
         return(
@@ -251,11 +309,11 @@ class Register extends Component {
                         />
                     }
                     buttonStyle={{backgroundColor:'red'}}
-
+                    onPress={this._signIn}
                 />
             </View>
             <View style={{flex:1}}>
-                <Button
+                {/* <Button
                     icon={
                         <Icon 
                             name='facebook'
@@ -264,7 +322,49 @@ class Register extends Component {
                         />
                     }
                     buttonStyle={{backgroundColor:'blue'}}
-                />
+                    onPress={ () =>
+                        AccessToken.getCurrentAccessToken()
+                        .then((data)=>{
+                            if(data!== null){
+                                console.log(data)
+
+
+                            }
+                            // else{
+                            //     LoginManager.setLoginBehavior('native_with_fallback')
+                            //     LoginManager.logInWithPermissions(["public_profile", "email", "user_photos"])
+                            //     .then((res)=>{
+                            //         console.log(res.grantedPermissions.toString())
+                            //     })
+                            //     .catch((err)=>{
+                            //         console.log(err)
+                            //     })
+                            // }
+                        })
+                        .catch((err)=>{
+                            console.log(err.message)
+                        })
+                    }
+                /> */}
+                <LoginButton
+                    publishPermissions={['publish_actions']}
+                    readPermissions={['public_profile']}
+                    onLoginFinished={
+                        (error, result) => {
+                        if (error) {
+                            console.log('login has error: ', result.error)
+                        } else if (result.isCancelled) {
+                            console.log('login is cancelled.')
+                        } else {
+                            AccessToken.getCurrentAccessToken().then((data) => {
+                            const { accessToken } = data
+                            this.initUser(accessToken)
+                            })
+                        }
+                        }
+                    }
+                    />
+                
             </View>
         </View>
         <View style={{flexDirection:'row',justifyContent:'center',marginTop:15}}>
